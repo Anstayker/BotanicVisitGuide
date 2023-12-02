7e20ca7a61434d19fd70ffdde2503964e05c948c
Anstayker <Christian.rojas.bl@gmail.com>
2023-12-02 01:27:11 -0400

    missing test for add_zone

---
+++ test/features/zone_creator_test.dart/domain/usecases/add_zone_test.dart
@@ -0,0 +1,39 @@
+import 'package:botanic_visit_guide/features/zone_creator/domain/entities/waypoint.dart';
+import 'package:botanic_visit_guide/features/zone_creator/domain/entities/zone_info.dart';
+import 'package:botanic_visit_guide/features/zone_creator/domain/repositories/zone_repository.dart';
+import 'package:botanic_visit_guide/features/zone_creator/domain/usecases/add_zone.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mocktail/mocktail.dart';
+
+class MockZoneRepository extends Mock implements ZoneRepository {}
+
+void main() {
+  late MockZoneRepository mockZoneRepository;
+  late AddZone addZone;
+
+  setUp(() {
+    mockZoneRepository = MockZoneRepository();
+    addZone = AddZone(mockZoneRepository);
+  });
+
+  const tWaypoiont = Waypoint(waypointId: 1, latitude: 1.0, longitude: 1.0);
+  const tZoneInfo =
+      ZoneInfo(zoneId: 1, name: 'Zona 1', waypoints: [tWaypoiont]);
+
+  test(
+    "should add a zone to the repository",
+    () async {
+      // arrange
+      when(() => mockZoneRepository.addZone(tZoneInfo))
+          .thenAnswer((_) async => const Right(null));
+      // act
+      final result = await addZone(const Params(zone: tZoneInfo));
+      // assert
+      expect(result, const Right(null));
+      verify(() => mockZoneRepository.addZone(tZoneInfo));
+      verifyNoMoreInteractions(mockZoneRepository);
+    },
+  );
+}
